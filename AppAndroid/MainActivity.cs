6deafeefacc20a47820e5812202f308a0ffe907d
Raushaniia Badrutdinova <Akvelon@DESKTOP-U6QNQBU>
2017-06-08 18:13:49 +0300

    my first commit

---
+++ AppAndroid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+
+namespace AppAndroid
+{
+	[Activity(Label = "AppAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			MobileCenter.Start("db1464ce-16a8-4864-a2eb-5363f5521f42", typeof(Analytics));
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+			button.Click += delegate { button.Text = string.Format("{0} Ru clicks!", count++); };
+		}
+	}
+}
+
